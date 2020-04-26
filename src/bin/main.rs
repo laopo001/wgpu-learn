ea364f073adabd4dd326b2d831be796caabe07ef
laopo001 <353272497@qq.com>
2020-04-27 03:00:39 +0800

    fix

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,17 +2,17 @@
 #[macro_use]
 extern crate lazy_static;
 
-use std::sync::Mutex;
+// use std::sync::Mutex;
 use wgpu_learn::{
     app,
     config::{Config, Event},
     console_log, time, Matrix4F32,
 };
 
-lazy_static! {
-    /// This is an example for using doc comment attributes
-    static ref ARR: Mutex<Vec<i32>> = Mutex::new(vec![]);
-}
+// lazy_static! {
+//     /// This is an example for using doc comment attributes
+//     static ref ARR: Mutex<Vec<i32>> = Mutex::new(vec![]);
+// }
 
 fn main() {
     let mut app = app::App::new("123", Config::PowerHighPerformance);
@@ -21,18 +21,18 @@ fn main() {
         1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0,
     );
     dbg!(m);
-    app.on(Event::Start, |app| {
-        ARR.lock().unwrap().push(1);
-    });
-    app.on(Event::End, |app| {
-        dbg!(ARR.lock().unwrap());
-    });
+    // app.on(Event::Start, |app| {
+    //     ARR.lock().unwrap().push(1);
+    // });
+    // app.on(Event::End, |app| {
+    //     dbg!(ARR.lock().unwrap());
+    // });
 
-    app.on(Event::Start, |app| {
-        app.array.push(1);
-    });
-    app.on(Event::End, |app| {
-        dbg!(app.array.len());
-    });
+    // app.on(Event::Start, |app| {
+    //     app.array.push(1);
+    // });
+    // app.on(Event::End, |app| {
+    //     dbg!(app.array.len());
+    // });
     app.start();
 }
