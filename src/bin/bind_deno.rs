96525a165885f55737a03227bea74274b0711e25
laopo001 <353272497@qq.com>
2020-05-29 20:40:25 +0800

    add bind_deno.rs

---
+++ src/bin/bind_deno.rs
@@ -0,0 +1,23 @@
+// fn create_main_worker(
+//     global_state: GlobalState,
+//     main_module: ModuleSpecifier,
+// ) -> Result<MainWorker, ErrBox> {
+//     let state = State::new(global_state, None, main_module, false)?;
+
+//     let mut worker = MainWorker::new("main".to_string(), startup_data::deno_isolate_init(), state);
+
+//     {
+//         let (stdin, stdout, stderr) = get_stdio();
+//         let mut t = worker.resource_table.borrow_mut();
+//         t.add("stdin", Box::new(stdin));
+//         t.add("stdout", Box::new(stdout));
+//         t.add("stderr", Box::new(stderr));
+//     }
+
+//     worker.execute("bootstrap.mainRuntime()")?;
+//     Ok(worker)
+// }
+
+fn main() {
+    println!("bind deno");
+}
