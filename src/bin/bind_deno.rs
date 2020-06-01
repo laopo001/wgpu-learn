c894492bd526ca5c2d4e716002c6b6111106ed45
laopo001 <353272497@qq.com>
2020-06-01 16:34:13 +0800

    update deno v1.0.3

--- src/bin/bind_deno.rs
+++ src/bin/bind_deno.rs
@@ -1,26 +1,4 @@
-use deno_cli::global_state::GlobalState;
-use deno_cli::worker::MainWorker;
-use deno_core::ErrBox;
-use deno_core::ModuleSpecifier;
-fn create_main_worker(
-    global_state: GlobalState,
-    main_module: ModuleSpecifier,
-) -> Result<MainWorker, ErrBox> {
-    let state = State::new(global_state, None, main_module, false)?;
-
-    let mut worker = MainWorker::new("main".to_string(), startup_data::deno_isolate_init(), state);
-
-    {
-        let (stdin, stdout, stderr) = get_stdio();
-        let mut t = worker.resource_table.borrow_mut();
-        t.add("stdin", Box::new(stdin));
-        t.add("stdout", Box::new(stdout));
-        t.add("stderr", Box::new(stderr));
-    }
-
-    worker.execute("bootstrap.mainRuntime()")?;
-    Ok(worker)
-}
+use deno_cli::create_main_worker;
 
 fn main() {
     println!("bind deno");
