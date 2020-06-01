d92beee49522f494cffcb2856532a1a7992e9324
laopo001 <353272497@qq.com>
2020-06-01 18:02:04 +0800

    完成加入deno编译

---
+++ main.ts
@@ -0,0 +1,2 @@
+import "https://deno.land/std/examples/welcome.ts";
+console.log("Welcome to Deno 🦕 123");
--- src/bin/bind_deno.rs
+++ src/bin/bind_deno.rs
@@ -1,5 +1,15 @@
-use deno_cli::create_main_worker;
+use deno_cli::flags;
+use deno_cli::run_command;
+use deno_cli::tokio_util;
 
 fn main() {
+    let args: Vec<String> = vec![
+        "deno".to_string(),
+        "run".to_string(),
+        "./main.ts".to_string(),
+    ];
+    let flags = flags::flags_from_vec(args);
+    let f = run_command(flags, "./main.ts".to_string());
+    tokio_util::run_basic(f);
     println!("bind deno");
 }
